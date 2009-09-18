36f0a3ac4519b30973d0c381d7ca3da9c5baed15
James Coglan <jcoglan@googlemail.com>
2009-09-18 18:39:50 +0100

    Ignore test/packages.js

--- test/packages.js
+++
@@ -1,7 +0,0 @@
-JS.Packages(function() { with(this) {
-
-    file('./lib/location-picker-min.js')
-        .provides("LocationPicker")
-        .requires("JS.MethodChain", "JS.Class", "GMap2", "GClientGeocoder", "GEvent", "GLatLng", "GMarker")
-        .uses("Ojay", "Ojay.HTML");
-}});
